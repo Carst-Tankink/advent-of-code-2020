624e8729a018c19aa6c8566bc2cbe70574156507
Carst Tankink <ctankink@bol.com>
2020-12-01 11:19:57 +0100

    AoC-2020 Day 1 Refactoring
    Use extension function for combinations, rename to combine2

--- src/main/kotlin/day1/ReportRepair.kt
+++ src/main/kotlin/day1/ReportRepair.kt
@@ -8,9 +8,9 @@ class ReportRepair {
             .readText()
             .lines()
             .filterNot { it.isEmpty() }
-            .map { x-> x.toLong() }
+            .map { x -> x.toLong() }
 
-        val combinations: Set<Pair<Long, Long>> = combine(input)
+        val combinations: Set<Pair<Long, Long>> = input.combine2()
 
         return combinations
             .filter { it.first + it.second == 2020L }
@@ -18,13 +18,13 @@ class ReportRepair {
             .first()
     }
 
-    fun solveStar2(inputFile: String) : Long {
+    fun solveStar2(inputFile: String): Long {
         val input: List<Long> = this.javaClass
             .getResource(inputFile)
             .readText()
             .lines()
             .filterNot { it.isEmpty() }
-            .map { x-> x.toLong() }
+            .map { x -> x.toLong() }
 
         val combinations: Set<Triple<Long, Long, Long>> = combine3(input)
 
@@ -34,28 +34,28 @@ class ReportRepair {
             .first()
     }
 
-    private fun combine(input: List<Long>): Set<Pair<Long, Long>> {
-        tailrec fun rec(acc: Set<Pair<Long, Long>>, remaining: List<Long>): Set<Pair<Long, Long>> {
+    private fun <T> List<T>.combine2(): Set<Pair<T, T>> {
+        tailrec fun rec(acc: Set<Pair<T, T>>, remaining: List<T>): Set<Pair<T, T>> {
             return if (remaining.isEmpty()) acc else {
-                val head: Long = remaining.first()
+                val head: T = remaining.first()
                 val tail = remaining.drop(1)
 
-                rec(acc + tail.map { Pair(head, it)}, tail)
+                rec(acc + tail.map { Pair(head, it) }, tail)
             }
         }
 
-        return rec(emptySet(), input)
+        return rec(emptySet(), this)
     }
 
     private fun combine3(input: List<Long>): Set<Triple<Long, Long, Long>> {
         tailrec fun rec(acc: Set<Triple<Long, Long, Long>>, remaining: List<Long>): Set<Triple<Long, Long, Long>> {
-            return if(remaining.isEmpty()) acc else {
+            return if (remaining.isEmpty()) acc else {
                 val head = remaining.first()
                 val tail = remaining.drop(1)
 
-                val combinations = combine(tail)
+                val combinations = tail.combine2()
 
-                rec(acc + combinations.map { Triple(head, it.first, it.second)}, tail)
+                rec(acc + combinations.map { Triple(head, it.first, it.second) }, tail)
             }
         }
 
