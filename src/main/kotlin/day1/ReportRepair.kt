f9da38b202d4a3a0078cdb2410d88255532fef8d
Carst Tankink <ctankink@bol.com>
2020-12-01 07:53:01 +0100

    AoC-2020 Day 1 Star 2
    Also add driver

--- src/main/kotlin/day1/ReportRepair.kt
+++ src/main/kotlin/day1/ReportRepair.kt
@@ -1,22 +1,37 @@
 package day1
 
-import java.io.File
-
 class ReportRepair {
 
-    fun solve(inputFile: String): Long {
-        val input: List<Long> = inputFile.lines()
+    fun solveStar1(inputFile: String): Long {
+        val input: List<Long> = this.javaClass
+            .getResource(inputFile)
+            .readText()
+            .lines()
             .filterNot { it.isEmpty() }
             .map { x-> x.toLong() }
 
         val combinations: Set<Pair<Long, Long>> = combine(input)
 
         return combinations
-            .filterNot { it.first == it.second  }
             .filter { it.first + it.second == 2020L }
             .map { it.first * it.second }
             .first()
+    }
 
+    fun solveStar2(inputFile: String) : Long {
+        val input: List<Long> = this.javaClass
+            .getResource(inputFile)
+            .readText()
+            .lines()
+            .filterNot { it.isEmpty() }
+            .map { x-> x.toLong() }
+
+        val combinations: Set<Triple<Long, Long, Long>> = combine3(input)
+
+        return combinations
+            .filter { it.first + it.second + it.third == 2020L }
+            .map { it.first * it.second * it.third }
+            .first()
     }
 
     private fun combine(input: List<Long>): Set<Pair<Long, Long>> {
@@ -26,6 +41,21 @@ class ReportRepair {
                 val tail = remaining.drop(1)
 
                 rec(acc + tail.map { Pair(head, it)}, tail)
+            }
+        }
+
+        return rec(emptySet(), input)
+    }
+
+    private fun combine3(input: List<Long>): Set<Triple<Long, Long, Long>> {
+        tailrec fun rec(acc: Set<Triple<Long, Long, Long>>, remaining: List<Long>): Set<Triple<Long, Long, Long>> {
+            return if(remaining.isEmpty()) acc else {
+                val head = remaining.first()
+                val tail = remaining.drop(1)
+
+                val combinations = combine(tail)
+
+                rec(acc + combinations.map { Triple(head, it.first, it.second)}, tail)
             }
         }
 
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    val day1 = day1.ReportRepair()
+
+    println("Sample: ${day1.solveStar1("/day1/sample")}")
+    println("Solution: ${day1.solveStar1("/day1/input")}")
+    println("Sample star2 : ${day1.solveStar2("/day1/sample")}")
+    println("Solution star2 : ${day1.solveStar2("/day1/input")}")
+}
