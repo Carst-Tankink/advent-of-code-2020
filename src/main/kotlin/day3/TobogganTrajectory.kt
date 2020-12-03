4c56ba07f6f8b1e5242735465571aaf485d20441
Carst Tankink <ctankink@bol.com>
2020-12-03 08:04:11 +0100

    AoC-2020 Day 3 Star 2

--- src/main/kotlin/day3/TobogganTrajectory.kt
+++ src/main/kotlin/day3/TobogganTrajectory.kt
@@ -3,22 +3,32 @@ package day3
 import structure.Solution
 
 
-class TobogganTrajectory(fileName: String) : Solution<List<Int>, Int>(fileName) {
-    override fun parse(line: String): List<Int> {
-        return line.map { if (it == '#')  1 else 0 }
+class TobogganTrajectory(fileName: String) : Solution<List<Long>, Long>(fileName) {
+    override fun parse(line: String): List<Long> {
+        return line.map { if (it == '#') 1 else 0 }
     }
 
-    override fun List<List<Int>>.solve1(): Int {
-        tailrec fun rec(acc: Int, x: Int, y: Int): Int {
+    override fun List<List<Long>>.solve1(): Long {
+        return treesHit(3, 1)
+    }
+
+    private fun List<List<Long>>.treesHit(slopeX: Int, slopeY: Int): Long {
+        tailrec fun rec(acc: Long, x: Int, y: Int): Long {
             return if (y >= this.size) acc else {
-                rec (acc + this[y][x], (x + 3) % this[y].size, y + 1)
+                rec(acc + this[y][x], (x + slopeX) % this[y].size, y + slopeY)
             }
         }
 
         return rec(0, 0, 0)
     }
 
-    override fun List<List<Int>>.solve2(): Int {
-        TODO("Not yet implemented")
+    override fun List<List<Long>>.solve2(): Long {
+        val options: List<Pair<Int, Int>> = listOf(
+            Pair(1, 1), Pair(3, 1), Pair(5, 1), Pair(7, 1), Pair(1, 2)
+        )
+
+        return options
+            .map { treesHit(it.first, it.second) }
+            .fold(1, { acc, i -> acc * i })
     }
 }
