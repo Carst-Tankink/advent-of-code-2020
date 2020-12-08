f8bb5661124040a1aad9f0f34a41f88ee22c2aac
Carst Tankink <ctankink@bol.com>
2020-12-08 08:32:32 +0100

    Day 8 Star 2

--- src/main/kotlin/day8/HandheldHalting.kt
+++ src/main/kotlin/day8/HandheldHalting.kt
@@ -2,6 +2,10 @@ package day8
 
 import util.Solution
 
+sealed class ExecutionResult(val accumulator: Long)
+class Loop(accumulator: Long) : ExecutionResult(accumulator)
+class Halt(accumulator: Long) : ExecutionResult(accumulator)
+
 data class Instruction(val operation: String, val argument: Int)
 
 class HandheldHalting(fileName: String) : Solution<Instruction, Long>(fileName) {
@@ -10,23 +14,49 @@ class HandheldHalting(fileName: String) : Solution<Instruction, Long>(fileName) 
         return Instruction(parts[0], parts[1].toInt())
     }
 
-    override fun List<Instruction>.solve1(): Long {
-        tailrec fun execute(accumulator: Long, programCounter: Int, executed: Set<Int>): Long {
-            return if (programCounter in executed) accumulator else {
-                val instruction = this[programCounter]
-                val newExecuted = executed + setOf(programCounter)
-                when (instruction.operation)  {
-                    "acc" -> execute(accumulator + instruction.argument, programCounter + 1, newExecuted)
-                    "jmp" -> execute(accumulator, programCounter + instruction.argument, newExecuted)
-                    "nop" -> execute(accumulator, programCounter + 1, newExecuted)
-                    else -> execute(accumulator, programCounter +1, newExecuted) // NOP
+    private fun runProgram(program: List<Instruction>): ExecutionResult {
+        tailrec fun execute(accumulator: Long, programCounter: Int, executed: Set<Int>): ExecutionResult {
+            return when {
+                programCounter in executed -> Loop(accumulator)
+                programCounter >= program.size -> Halt(accumulator)
+                else -> {
+                    val instruction = program[programCounter]
+                    val newExecuted = executed + setOf(programCounter)
+                    when (instruction.operation) {
+                        "acc" -> execute(accumulator + instruction.argument, programCounter + 1, newExecuted)
+                        "jmp" -> execute(accumulator, programCounter + instruction.argument, newExecuted)
+                        "nop" -> execute(accumulator, programCounter + 1, newExecuted)
+                        else -> execute(accumulator, programCounter + 1, newExecuted) // NOP
+                    }
                 }
             }
         }
+
         return execute(0, 0, emptySet())
     }
 
+    override fun List<Instruction>.solve1(): Long {
+        return runProgram(this).accumulator
+    }
+
+    private fun flipAt(program: List<Instruction>, index: Int): List<Instruction> {
+        return program.mapIndexed { idx, entry ->
+            when {
+                idx != index -> entry
+                entry.operation == "jmp" -> entry.copy(operation = "nop")
+                entry.operation == "nop" -> entry.copy(operation = "jmp")
+                else -> entry
+            }
+        }
+    }
+
     override fun List<Instruction>.solve2(): Long {
-        TODO("Not yet implemented")
+        return withIndex()
+            .filter { v -> v.value.operation == "nop" || v.value.operation == "jmp" }
+            .map { it.index }
+            .map { index -> flipAt(this, index) }
+            .map { runProgram(it) }
+            .find { it is Halt }
+            ?.accumulator ?: -1
     }
 }
