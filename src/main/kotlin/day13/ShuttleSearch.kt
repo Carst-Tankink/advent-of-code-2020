1ecf2a032c7ceaf45e357c27d480eabfc13b857c
Carst Tankink <ctankink@bol.com>
2020-12-13 19:18:11 +0100

    Day 13 Star 2 Solved
    This will make my laptop overheat, missing the kickstarter

--- src/main/kotlin/day13/ShuttleSearch.kt
+++ src/main/kotlin/day13/ShuttleSearch.kt
@@ -2,26 +2,44 @@ package day13
 
 import util.Solution
 
-class ShuttleSearch(fileName: String) : Solution<List<Long>, Long>(fileName) {
-    override fun parse(line: String): List<Long> {
+class ShuttleSearch(fileName: String) : Solution<List<IndexedValue<Long>>, Long>(fileName) {
+    override fun parse(line: String): List<IndexedValue<Long>> {
         return line
             .split(',')
-            .filterNot { it == "x" }
-            .map { it.toLong() }
-
+            .withIndex()
+            .filter { it.value != "x" }
+            .map { IndexedValue(it.index, it.value.toLong()) }
     }
 
-    override fun List<List<Long>>.solve1(): Long {
-        val map = this[1]
-            .map { id -> Pair(id, id - (this[0][0] % id)) }
-        val earliestShuttle: Pair<Long, Long> = map
-            .minByOrNull { it.second} ?: Pair(-1, -1)
+    override fun List<List<IndexedValue<Long>>>.solve1(): Long {
+        val earliestShuttle: Pair<Long, Long> = this[1]
+            .map { it.value }
+            .map { id -> Pair(id, id - ((this[0][0].value % id))) }
+            .minByOrNull { it.second } ?: Pair(-1, -1)
 
         return earliestShuttle.first * earliestShuttle.second
-
     }
 
-    override fun List<List<Long>>.solve2(): Long {
-        TODO("Not yet implemented")
+    override fun List<List<IndexedValue<Long>>>.solve2(): Long {
+        println("List ${this[1]}")
+        return this[1]
+            .lcm()
+    }
+
+    private fun List<IndexedValue<Long>>.lcm(): Long {
+        val increments: Map<Int, Long> = this.groupBy { it.index }.mapValues { it.value.first().value }
+        tailrec fun rec(current: List<IndexedValue<Long>>): Long {
+            val first = current.first()
+            val minimal = current.minByOrNull { it.value }!!
+            return if (current.map { (it.value - first.value) == it.index.toLong() }.all { it }) first.value else {
+                rec(current.map {
+                    if (it.index == minimal.index) IndexedValue(
+                        it.index, it.value + (increments[it.index] ?: error(""))
+                    ) else it
+                })
+            }
+        }
+
+        return rec(this)
     }
 }
