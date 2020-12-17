e614a7e72ad11d0f04981d006d90a450cbe63bb0
Carst Tankink <ctankink@bol.com>
2020-12-17 09:31:14 +0100

    Day 17 star 2
    That took embarassingly long

--- src/main/kotlin/day17/ConwayCubes.kt
+++ src/main/kotlin/day17/ConwayCubes.kt
@@ -2,21 +2,22 @@ package day17
 
 import util.Solution
 
-data class Location(val x: Int, val y: Int, val z: Int) {
-    fun neighbours(): List<Location> {
-        return listOf(
-            copy(x = x - 1, y = y - 1, z = z - 1), copy(y = y - 1, z = z - 1), copy(x = x + 1, y = y - 1, z = z - 1),
-            copy(x = x - 1, z = z - 1), copy(z = z - 1), copy(x = x + 1, z = z - 1),
-            copy(x = x - 1, y = y + 1, z = z - 1), copy(y = y + 1, z = z - 1), copy(x = x + 1, y = y + 1, z = z - 1),
+data class Location(val x: Int, val y: Int, val z: Int, val w: Int) {
 
-            copy(x = x - 1, y = y - 1), copy(y = y - 1), copy(x = x + 1, y = y - 1),
-            copy(x = x - 1), copy(x = x + 1),
-            copy(x = x - 1, y = y + 1), copy(y = y + 1), copy(x = x + 1, y = y + 1),
-
-            copy(x = x - 1, y = y - 1, z = z + 1), copy(y = y - 1, z = z + 1), copy(x = x + 1, y = y - 1, z = z + 1),
-            copy(x = x - 1, z = z + 1), copy(z = z + 1), copy(x = x + 1, z = z + 1),
-            copy(x = x - 1, y = y + 1, z = z + 1), copy(y = y + 1, z = z + 1), copy(x = x + 1, y = y + 1, z = z + 1),
-        )
+    fun neighbours(includeW: Boolean = false): List<Location> {
+        val xNeighbours: List<Int> = listOf(x - 1, x, x + 1)
+        val yNeighbours: List<Int> = listOf(y - 1, y, y + 1)
+        val zNeighbours: List<Int> = listOf(z - 1, z, z + 1)
+        val wNeighbours: List<Int> = if (includeW) listOf(w - 1, w, w + 1) else listOf(w)
+        return xNeighbours.flatMap { newX ->
+            yNeighbours.flatMap { newY ->
+                zNeighbours.flatMap { newZ ->
+                    wNeighbours.map { newW ->
+                        Location(newX, newY, newZ, newW)
+                    }
+                }
+            }
+        }.filterNot { it == this }
     }
 }
 
@@ -25,36 +26,36 @@ class ConwayCubes(fileName: String) : Solution<List<Boolean>, Long>(fileName) {
         return line.map { it == '#' }
     }
 
+    private fun update(active: Set<Location>, includeW: Boolean): Set<Location> {
+        val toConsider = active.flatMap { it.neighbours(includeW) }.toSet()
+        return toConsider.mapNotNull { loc ->
+            val activeNeighbours = loc.neighbours(includeW).count { it in active }
+            when {
+                loc in active && activeNeighbours in 2..3 -> loc
+                loc !in active && activeNeighbours == 3 -> loc
+                else -> null
+            }
+        }
+            .toSet()
+    }
+
+    private tailrec fun cycle(current: Int, locations: Set<Location>, includeW: Boolean): Set<Location> {
+        return if (current == 6) locations else {
+            cycle(current + 1, update(locations, includeW), includeW)
+        }
+    }
+
+    private fun List<List<Boolean>>.getInitialActive() =
+        this.flatMapIndexed { y, line -> line.mapIndexed { x, b -> if (b) Location(x, y, 0, 0) else null } }
+            .mapNotNull { it }
+            .toSet()
+
     override fun List<List<Boolean>>.solve1(): Long {
-        val stop = 6
-        fun update(active: Set<Location>): Set<Location> {
-            val toConsider = active.flatMap { it.neighbours() }.toSet()
-            return toConsider.mapNotNull { loc ->
-                val activeNeighbours = loc.neighbours().count { it in active }
-                when {
-                    loc in active && activeNeighbours in 2..3 -> loc
-                    loc !in active && activeNeighbours == 3 -> loc
-                    else -> null
-                }
-            }
-                .toSet()
-        }
-
-        tailrec fun cycle(current: Int, locations: Set<Location>): Set<Location> {
-            return if (current == stop) locations else {
-                cycle(current + 1, update(locations))
-            }
-        }
-
-        val initialPositions: Set<Location> =
-            this.flatMapIndexed { y, line -> line.mapIndexed { x, b -> if (b) Location(x, y, 0) else null } }
-                .mapNotNull { it }
-                .toSet()
-        return cycle(0, initialPositions).size.toLong()
+        return cycle(0, getInitialActive(), false).size.toLong()
 
     }
 
     override fun List<List<Boolean>>.solve2(): Long {
-        TODO("Not yet implemented")
+        return cycle(0, getInitialActive(), true).size.toLong()
     }
 }
