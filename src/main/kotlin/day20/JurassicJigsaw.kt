b5a2dd907dffc221e12a82f57ef117d705738715
Carst Tankink <ctankink@bol.com>
2020-12-22 09:10:12 +0100

    Day 20 Star 2 partial
    The puzzle solving is done, still need to stitch to full image and detect monsters

--- src/main/kotlin/day20/JurassicJigsaw.kt
+++ src/main/kotlin/day20/JurassicJigsaw.kt
@@ -2,6 +2,7 @@ package day20
 
 import util.Solution
 import util.product
+import kotlin.math.sqrt
 
 sealed class TilePart
 data class TileId(val id: Long) : TilePart()
@@ -18,6 +19,34 @@ data class Tile(val id: Long, val lines: List<String>) {
     }
 
     fun edges() = listOf(top, bottom, left, right)
+
+    fun variants(): Set<Tile> {
+        return (this.rotations() + this.flip().rotations()).toSet()
+    }
+
+    private fun flip(): Tile {
+        return copy(lines = lines.indices.map { y ->
+            lines[(lines.size - 1) - y]
+        })
+    }
+
+    private fun rotateRight(): Tile {
+        return copy(lines = lines.indices.map { y ->
+            lines[y].indices.map { x ->
+                lines[(lines.size - 1) - x][y]
+            }.joinToString("")
+        })
+    }
+
+    private fun rotations(): List<Tile> {
+        return listOf(
+            this,
+            this.rotateRight(),
+            this.rotateRight().rotateRight(),
+            this.rotateRight().rotateRight().rotateRight()
+        )
+    }
+
 }
 
 class JurassicJigsaw(fileName: String) : Solution<TilePart, Long>(fileName) {
@@ -49,20 +78,108 @@ class JurassicJigsaw(fileName: String) : Solution<TilePart, Long>(fileName) {
 
     override fun List<TilePart>.solve1(): Long {
         val tiles = assembleTiles(this)
-
-        val sampleCorner = tiles.find { it.id == 3079L } ?: error("No such tile")
-        val neighbours = getNeighbours(sampleCorner, tiles - sampleCorner)
-        return tiles
-            .filter { getNeighbours(it, tiles - it).size == 2 }
+        return findCorners(tiles)
             .map { it.id }
             .product()
     }
+
+    private fun findCorners(tiles: List<Tile>) = tiles
+        .filter { getNeighbours(it, tiles - it).size == 2 }
 
     private fun getNeighbours(current: Tile, others: List<Tile>): List<Tile> {
         return others.filter { it.edgeConfigurations().any { config -> config in current.edges() } }
     }
 
+    private fun neighbour(side: String, others: List<Tile>): Tile? {
+        val neighbours = others.filter { side in it.edgeConfigurations() }
+        if (neighbours.size > 1) error("More than one tile")
+        return others.find { side in it.edgeConfigurations() }
+    }
+
     override fun List<TilePart>.solve2(): Long {
+        val tiles = assembleTiles(this)
+        val puzzle = assembleImage(tiles)
+        val image = stitchImage(puzzle)
+        return -1
+    }
+
+    private fun stitchImage(puzzle: List<List<Tile>>): Tile {
+        val strippedBorders: List<List<Tile>> = puzzle.flatMap { line ->
+            line.map {
+                val body = it.lines.subList(1, it.lines.size - 1).map { s -> s.substring(1, s.length - 1) }
+                return Tile(it.id, body)
+            }
+        }
+
+
         TODO("Not yet implemented")
+    }
+
+    private fun assembleImage(tiles: List<Tile>): List<List<Tile>> {
+        val sideLength = sqrt(tiles.size.toDouble()).toInt()
+
+        val firstCorner = findCorners(tiles).first()
+        val others = tiles - firstCorner
+
+        val corner = firstCorner.variants().find {
+            neighbour(it.top, others) == null && neighbour(it.left, others) == null
+        } ?: error("No corner")
+
+        val tilePositions = (0 until sideLength).flatMap { y ->
+            (0 until sideLength).map { x -> Pair(y, x) }
+        }
+
+        tailrec fun build(
+            acc: List<List<Tile>>,
+            current: List<Tile>,
+            remainingTiles: List<Tile>,
+            positions: List<Pair<Int, Int>>
+        ): List<List<Tile>> {
+            return if (positions.isEmpty()) acc + listOf(current) else {
+                val (y, x) = positions.first()
+                val (newAcc, newCurrent, newRemaining) = when {
+                    y == 0 && x == 0 -> Triple(
+                        acc,
+                        current + corner,
+                        remainingTiles.filterNot { it.id == corner.id })
+                    y == 0 && x < sideLength -> {
+                        val previousTile = current[x - 1].right
+                        val matchingTile = neighbour(previousTile, remainingTiles) ?: error("No tile found")
+                        val orientation = matchingTile.variants()
+                            .find { it.left == previousTile } ?: error("No orientation")
+                        Triple(acc, current + orientation, remainingTiles.filterNot { it.id == matchingTile.id })
+                    }
+                    y < sideLength && x == 0 -> {
+                        val previousTile = current.first().bottom
+                        val matchingTile = neighbour(previousTile, remainingTiles) ?: error("No tile found")
+                        val orientation = matchingTile.variants()
+                            .find { it.top == previousTile } ?: error("No orientation")
+                        Triple(
+                            acc + listOf(current),
+                            listOf(orientation),
+                            remainingTiles.filterNot { it.id == matchingTile.id })
+                    }
+                    else -> {
+                        val left = current[x - 1].right
+                        val above = acc[y-1][x].bottom
+                        val matchingTile = neighbour(left, remainingTiles) ?: error("No tile found")
+                        val orientation = matchingTile.variants()
+                            .find { it.left == left } ?: error("No orientation")
+
+                        if (orientation.top != above) {
+                            println("Placed a tile in the wrong spot! ${orientation.id}")
+                        }
+                        Triple(
+                            acc,
+                            current + orientation,
+                            remainingTiles.filterNot { it.id == matchingTile.id })
+                    }
+                }
+
+                build(newAcc, newCurrent, newRemaining, positions.drop(1))
+            }
+        }
+
+        return build(emptyList(), emptyList(), tiles, tilePositions)
     }
 }
