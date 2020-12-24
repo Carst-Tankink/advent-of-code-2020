c28d8b6a07e9cac86dcf7eeb7ef963355456dc77
Carst Tankink <ctankink@bol.com>
2020-12-24 19:56:20 +0100

    Day 24 Star 2

--- src/main/kotlin/day24/LobbyLayout.kt
+++ src/main/kotlin/day24/LobbyLayout.kt
@@ -12,8 +12,8 @@ enum class HexDirection {
 }
 
 class LobbyLayout(fileName: String) : Solution<List<HexDirection>, Long>(fileName) {
-    override fun parse(line: String): List<HexDirection>? {
-        return line.fold(Pair(emptyList<HexDirection>(), null as Char?)) { (acc, vertical), c ->
+    override fun parse(line: String): List<HexDirection>? =
+        line.fold(Pair(emptyList<HexDirection>(), null as Char?)) { (acc, vertical), c ->
             when (c) {
                 'e' -> {
                     val dir = when (vertical) {
@@ -39,10 +39,11 @@ class LobbyLayout(fileName: String) : Solution<List<HexDirection>, Long>(fileNam
                 else -> error("Unexpected character")
             }
         }.first
-    }
 
-    override fun List<List<HexDirection>>.solve1(): Long {
-        val totalSet: Set<Triple<Int, Int, Int>> = this.fold(emptySet()) { blackTiles, steps ->
+    override fun List<List<HexDirection>>.solve1(): Long = initialTiling().size.toLong()
+
+    private fun List<List<HexDirection>>.initialTiling(): Set<Triple<Int, Int, Int>> =
+        fold(emptySet()) { blackTiles, steps ->
             val nextPosition = steps.fold(Triple(0, 0, 0)) { (x, y, z), step ->
                 when (step) {
                     HexDirection.NORTH_EAST -> Triple(x + 1, y, z - 1)
@@ -60,10 +61,44 @@ class LobbyLayout(fileName: String) : Solution<List<HexDirection>, Long>(fileNam
                 blackTiles + nextPosition
             }
         }
-        return totalSet.size.toLong()
-    }
 
     override fun List<List<HexDirection>>.solve2(): Long {
-        TODO("Not yet implemented")
+        fun getNeighbours(location: Triple<Int, Int, Int>): Set<Triple<Int, Int, Int>> {
+            val (x, y, z) = location
+            return setOf(
+                Triple(x + 1, y, z - 1),
+                Triple(x + 1, y - 1, z),
+                Triple(x, y - 1, z + 1),
+                Triple(x - 1, y, z + 1),
+                Triple(x - 1, y + 1, z),
+                Triple(x, y + 1, z - 1)
+            )
+        }
+
+        tailrec fun flip(day: Int, tiling: Set<Triple<Int, Int, Int>>): Set<Triple<Int, Int, Int>> {
+            return if (day == 100) tiling else {
+                val newWhite = tiling.filter { location ->
+                    val neighbours: Set<Triple<Int, Int, Int>> = getNeighbours(location)
+                    val blackNeighbours = neighbours.count { it in tiling }
+
+                    blackNeighbours == 0 || blackNeighbours > 2
+                }
+
+                val whiteNeighbours = tiling.flatMap { getNeighbours(it) }
+                    .filterNot { it in tiling }
+
+
+                val newBlack = whiteNeighbours.filter { n ->
+                    val neighbours = getNeighbours(n)
+                    val blackNeighbours = neighbours.count { it in tiling }
+
+                    blackNeighbours == 2
+                }
+
+                flip(day + 1, (tiling - newWhite) + newBlack)
+            }
+        }
+
+        return flip(0, initialTiling()).size.toLong()
     }
 }
